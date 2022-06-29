ab3dd3ca8a1783fff60dd49c696ec52a434fe0e9
Eric Zhang <erizha.grad2022@my.stmc.bc.ca>
2022-06-29 09:47:06 -0700

    Added date selector for street imagery
    - Date picker added to imagery page in settings
    - Database migrated with new column for the date

--- frontend/src/components/projectEdit/imageryForm.js
+++ frontend/src/components/projectEdit/imageryForm.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState, useLayoutEffect } from 'react';
 import Select from 'react-select';
 import { FormattedMessage } from 'react-intl';
-
+import DatePicker from 'react-datepicker';
 import messages from './messages';
 import { StateContext, styleClasses } from '../../views/projectEdit';
 import { Code } from '../code';
@@ -33,7 +33,9 @@ export const ImageryForm = () => {
           <FormattedMessage {...messages.imagery} />
         </label>
         <ImageryField imagery={projectInfo.imagery} setProjectInfo={setProjectInfo} />
+
       </div>
+
       <div className={styleClasses.divClass}>
         <label className={styleClasses.labelClass}>
           <FormattedMessage {...messages.license} />
@@ -54,6 +56,26 @@ export const ImageryForm = () => {
           className="w-50 z-1"
         />
       </div>
+
+      <div className={styleClasses.divClass}>
+        <label className={styleClasses.labelClass}>
+          Earliest street imagery capture
+        </label>
+        <DatePicker
+          selected={Date.parse(projectInfo.earliestStreetImagery)}
+          onChange={(date) =>
+            setProjectInfo({
+              ...projectInfo,
+              earliestStreetImagery: date,
+            })
+          }
+          dateFormat="dd/MM/yyyy"
+          className={styleClasses.inputClass}
+          showYearDropdown
+          scrollableYearDropdown
+        />
+      </div>
+
     </div>
   );
 };
