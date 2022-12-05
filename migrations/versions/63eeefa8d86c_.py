cbfb40b48fdc04e012d4db4459b7466d0e0beaed
Taylor Smock <tsmock@meta.com>
2022-12-05 11:57:40 -0700

    Merge MapWithAI TM head with HOT TM4 head
    This creates a merge revision between mwai and v4.7.4, v4.8, and v4.8.1
    
    Signed-off-by: Taylor Smock <tsmock@meta.com>

---
+++ migrations/versions/63eeefa8d86c_.py
@@ -0,0 +1,24 @@
+"""Merge head between mwai and v4.8
+
+Revision ID: 63eeefa8d86c
+Revises: 1c22007182e1, 6276c258149c
+Create Date: 2024-07-17 14:42:54.598123
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '63eeefa8d86c'
+down_revision = ('1c22007182e1', '6276c258149c')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
